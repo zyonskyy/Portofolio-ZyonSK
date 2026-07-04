4cc1e68f6b3584ac36cb97bb3ba8488c1e9aa150
ZyonSK <zyonskyy@gmail.com>
2026-07-04 15:34:45 +0700

    script.js

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
+/* NAVIGATION BAR */
 function scrollToContact() {
-    document.getElementById("contact").scrollIntoView({ behavior: 'smooth' });
+    document.getElementById("contact").scrollIntoView({ behavior: "smooth" });
 }
 
-document.getElementById("contactForm").addEventListener("submit", function(event) {
-  event.preventDefault();
-  document.getElementById("message").innerText = "Pesan Berhasil Dikirim, TerimaKasih";
-  this.reset();
+document
+    .getElementById("contactForm")
+    .addEventListener("submit", function (event) {
+        event.preventDefault();
+        document.getElementById("message").innerText =
+            "Pesan Berhasil Dikirim, TerimaKasih";
+        this.reset();
+    });
+
+const menuToggle = document.getElementById("menuToggle");
+const navMenu = document.getElementById("navMenu");
+
+menuToggle.addEventListener("click", () => {
+    navMenu.classList.toggle("active");
+});
+
+// Otomatis tutup menu setelah link diklik (khusus mobile)
+navMenu.querySelectorAll("a").forEach(link => {
+    link.addEventListener("click", () => {
+        navMenu.classList.remove("active");
+    });
 });
